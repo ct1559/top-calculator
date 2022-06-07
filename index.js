fd064d84e59248bffb5f30a77c5f27f9d344d44a
ct1559 <e.cooper.taylor@gmail.com>
2022-06-07 19:55:32 -0400

    Create needed files and add basic math functions

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(operand, num1, num2) {
+  switch (operand) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1 / num2);
+      break;
+  }
+}
