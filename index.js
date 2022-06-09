0925d0777b2536957d59d09a56d65ca26a000bbc
ct1559 <e.cooper.taylor@gmail.com>
2022-06-08 21:20:38 -0400

    Add event listeners to buttons and start logic
    Add logic to convert string ID of 0-9 buttons to
    numbers

--- index.js
+++ index.js
@@ -30,3 +30,39 @@ function operate(operand, num1, num2) {
       break;
   }
 }
+
+// Function to convert button input
+function convertBtnInput(str) {
+  switch (str) {
+    case "zero":
+      return 0;
+    case "one":
+      return 1;
+    case "two":
+      return 2;
+    case "three":
+      return 3;
+    case "four":
+      return 4;
+    case "five":
+      return 5;
+    case "six":
+      return 6;
+    case "seven":
+      return 7;
+    case "eight":
+      return 8;
+    case "nine":
+      return 9;
+  }
+}
+
+// Listen for Button Events
+const btn = document.querySelectorAll(".btn");
+btn.forEach(function (currentValue, currentIndex, listObj) {
+  currentValue.addEventListener("click", (e) => {
+    console.log(e.target.id);
+    console.log(convertBtnInput(e.target.id));
+    convertBtnInput(e.target.id);
+  });
+});
